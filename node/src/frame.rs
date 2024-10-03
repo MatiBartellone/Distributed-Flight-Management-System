91a232f513f27f773894aa1e01346732415a29e6
MatiBartellone <matias.bartellone@gmail.com>
2024-10-03 11:09:33 -0300

    request frame validator

--- node/src/frame.rs
+++ node/src/frame.rs
@@ -20,14 +20,23 @@ impl Frame {
         let length = cursor.read_u32()?;
         let body = cursor.read_remaining_bytes()?;
 
-        Ok(Frame {
+        let frame = Frame {
             version,
             flags,
             stream,
             opcode,
             length,
             body,
-        })
+        };
+        frame.validate_request_frame()?;
+        Ok(frame)
+    }
+
+    fn validate_request_frame(&self) -> Result<(), Errors> {
+        if self.version != 0x03 { return Err(Errors::ProtocolError(format!("Version {} is incorrect", self.version))); }
+        if self.flags != 0x00 { return Err(Errors::ProtocolError(format!("Flag {} is incorrect", self.flags))); }
+        if self.stream < 0x00 { return Err(Errors::ProtocolError(String::from("Stream cannot be negative"))); }
+        Ok(())
     }
 }
 
@@ -111,7 +120,7 @@ mod tests {
     #[test]
     fn test_parse_frame() -> Result<(), Errors> {
         let bytes = vec![
-            0x04,
+            0x03,
             0x00,
             0x00, 0x01,
             0x03,
@@ -119,7 +128,7 @@ mod tests {
             0x10, 0x03, 0x35, 0x12, 0x22
         ];
         let result = Frame::parse_frame(&bytes)?;
-        assert_eq!(result.version, 4);
+        assert_eq!(result.version, 3);
         assert_eq!(result.flags, 0);
         assert_eq!(result.stream, 1);
         assert_eq!(result.opcode, 3);
