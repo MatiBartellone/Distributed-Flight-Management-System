505dffae92168989f51cb13e647a131d31d4774c
Emiliano Gomez <emiliano2805@gmail.com>
2024-10-03 17:57:22 -0300

    options parser skel

--- node/src/parsers/query_parser.rs
+++ node/src/parsers/query_parser.rs
@@ -1,11 +1,13 @@
 use crate::executables::executable::Executable;
 use crate::executables::query_executable::QueryExecutable;
 use crate::parsers::parser::Parser;
+use crate::utils::errors::Errors;
 
 pub struct QueryParser;
 
 impl Parser for QueryParser {
-    fn parse(&self, _bytes: &[u8]) -> Box<dyn Executable> {
-        Box::new(QueryExecutable)
+    fn parse(&self, _bytes: &[u8]) -> Result<Box<dyn Executable>, Errors> {
+        Ok(Box::new(QueryExecutable))
     }
 }
+
--- node/src/parsers/startup_parser.rs
+++ node/src/parsers/startup_parser.rs
@@ -1,11 +1,13 @@
 use crate::executables::executable::Executable;
 use crate::executables::startup_executable::StartupExecutable;
 use crate::parsers::parser::Parser;
+use crate::utils::errors::Errors;
 
 pub struct StartupParser;
 
 impl Parser for StartupParser {
-    fn parse(&self, _bytes: &[u8]) -> Box<dyn Executable> {
-        Box::new(StartupExecutable)
+    fn parse(&self, _bytes: &[u8]) -> Result<Box<dyn Executable>, Errors> {
+        Ok(Box::new(StartupExecutable))
     }
 }
+
