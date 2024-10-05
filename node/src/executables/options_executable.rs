6af18e57e10ba24668bdde69b7636754aa94e13d
Emiliano Gomez <emiliano2805@gmail.com>
2024-10-05 15:15:40 -0300

    changes requested in PR done

--- node/src/executables/options_executable.rs
+++ node/src/executables/options_executable.rs
@@ -2,65 +2,82 @@ use std::collections::HashMap;
 
 use crate::executables::executable::Executable;
 use crate::frame::Frame;
+use crate::response_builders::frame_builder::FrameBuilder;
 
 const ARGUMENTS_QUANTITY: u8 = 0x02;
+const CQL_VERSION: &str = "3.0.0";
+const COMPRESSION: &str = "lz4";
 
-pub struct OptionsExecutable {
-    options: HashMap<String, String>,
+pub struct OptionsExecutable {}
+
+impl Default for OptionsExecutable {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl OptionsExecutable {
-    pub fn new(options: HashMap<String, String>) -> OptionsExecutable {
-        OptionsExecutable { options }
+    pub fn new() -> OptionsExecutable {
+        OptionsExecutable {}
+    }
+
+    fn get_options(&self) -> HashMap<String, String> {
+        let mut options = HashMap::<String, String>::new();
+        options.insert("CQL_VERSION".to_string(), CQL_VERSION.to_string());
+        options.insert("COMPRESSION".to_string(), COMPRESSION.to_string());
+
+        options
+    }
+
+    fn extend_multimap_with_string(&self, string_multimap: &mut Vec<u8>, element: &String) {
+        string_multimap.push(element.len() as u8);
+        string_multimap.extend_from_slice(element.as_bytes());
     }
 
     fn get_string_multimap(&self) -> Vec<u8> {
         let mut string_multimap = Vec::new();
-
+        let options = self.get_options();
         string_multimap.push(ARGUMENTS_QUANTITY);
 
-        for (key, value) in self.options.iter() {
-            string_multimap.extend_from_slice(key.as_bytes());
-            string_multimap.extend_from_slice(value.as_bytes());
+        for (key, value) in options.iter() {
+            self.extend_multimap_with_string(&mut string_multimap, key);
+            self.extend_multimap_with_string(&mut string_multimap, value);
         }
+
         string_multimap
     }
 }
 
 impl Executable for OptionsExecutable {
     fn execute(&self) -> Frame {
-        let version = 0x03;
-        let flags = 0x00;
-        let stream = 0x01;
-        let opcode = 0x06;
-        let body = self.get_string_multimap();
-        let length = body.len() as u32;
+        let new_body = self.get_string_multimap();
         let frame = Frame {
-            version,
-            flags,
-            stream,
-            opcode,
-            length,
-            body,
+            // TODO: Sacarlo una vez todos los parsers reciban el frame, asi
+            // se le pasa al FrameBuilder el frame viejo.
+            version: 0x03,
+            flags: 0x00,
+            stream: 0x01,
+            opcode: 0x05,
+            length: 0,
+            body: vec![],
         };
+        // TODO: Sacar el unwrap y manejar el error
 
-        frame
+        FrameBuilder::build_response_frame(frame, 0x06, new_body).unwrap()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    const EXPECTED_VERSION: u8 = 0x03;
+    const EXPECTED_VERSION: u8 = 131;
     const EXPECTED_FLAGS: u8 = 0x00;
     const EXPECTED_STREAM: i16 = 0x01;
     const EXPECTED_OPCODE: u8 = 0x06;
-    const EXPECTED_LENGTH: u32 = 31;
+    const EXPECTED_LENGTH: u32 = 35;
     use super::*;
+
     fn setup() -> OptionsExecutable {
-        let mut options = HashMap::<String, String>::new();
-        options.insert("CQL_VERSION".to_string(), "3.0.0".to_string());
-        options.insert("COMPRESSION".to_string(), "lz4".to_string());
-        let executable = OptionsExecutable::new(options);
+        let executable = OptionsExecutable::new();
         executable
     }
 
--- node/src/parsers/options_parser.rs
+++ node/src/parsers/options_parser.rs
@@ -1,41 +1,26 @@
-use std::collections::HashMap;
-
 use crate::executables::executable::Executable;
 use crate::executables::options_executable::OptionsExecutable;
 use crate::parsers::parser::Parser;
 use crate::utils::errors::Errors;
 
-const ARGUMENTS_LENGTH: usize = 5;
-const CQL_VERSION: &str = "3.0.0";
-const COMPRESSION: &str = "lz4";
-
 pub struct OptionsParser;
 
 impl OptionsParser {
     fn validate_empty_body(&self, bytes: &[u8]) -> Result<(), Errors> {
-        if bytes.len() != ARGUMENTS_LENGTH {
+        if !bytes.is_empty() {
             return Err(Errors::ProtocolError(String::from(
                 "Options body should be empty",
             )));
         }
         Ok(())
     }
-
-    fn get_options(&self) -> HashMap<String, String> {
-        let mut options = HashMap::<String, String>::new();
-        options.insert("CQL_VERSION".to_string(), CQL_VERSION.to_string());
-        options.insert("COMPRESSION".to_string(), COMPRESSION.to_string());
-
-        options
-    }
 }
 
 impl Parser for OptionsParser {
-    fn parse(&self, _bytes: &[u8]) -> Result<Box<dyn Executable>, Errors> {
-        self.validate_empty_body(_bytes)?;
-        let options = self.get_options();
+    fn parse(&self, body: &[u8]) -> Result<Box<dyn Executable>, Errors> {
+        self.validate_empty_body(body)?;
 
-        let executable = OptionsExecutable::new(options);
+        let executable = OptionsExecutable::new();
         Ok(Box::new(executable))
     }
 }
@@ -45,7 +30,7 @@ mod tests {
     use super::*;
     fn setup() -> (OptionsParser, Vec<u8>) {
         let parser = OptionsParser;
-        let bytes = vec![0x03, 0x00, 0x00, 0x01, 0x05];
+        let bytes = vec![];
         (parser, bytes)
     }
     #[test]
