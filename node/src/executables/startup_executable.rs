26e67480c034e0ec52eb81b22a363ccd47b0b1fb
ivan-maximoff <maximoff.ivan@gmail.com>
2024-10-05 15:57:22 -0300

    Test de startup parser y verificacion de compression

--- node/src/executables/startup_executable.rs
+++ node/src/executables/startup_executable.rs
@@ -2,7 +2,9 @@ use std::collections::HashMap;
 
 use crate::executables::executable::Executable;
 use crate::frame::Frame;
+use crate::utils::parser_constants::AUTHENTICATE;
 
+#[derive(Debug)]
 pub struct StartupExecutable {
     _config: HashMap<String, String>
 }
@@ -13,9 +15,12 @@ impl StartupExecutable {
     }
 }
 
-
 impl Executable for StartupExecutable {
+    /// Configurate the startup configurations and create a AUTHENTICATE response
     fn execute(&self) -> Frame {
         todo!()
+        // Aplicar configs
+        // let auth_class = vec![]; // HAY QUE ELEGIR AUTH CLASS Y PASARLA EN BYTES
+        // FrameBuilder::build_response_frame(request_frame, AUTHENTICATE, auth_class)
     }
 }
--- node/src/utils/conversion.rs
+++ node/src/utils/conversion.rs
@@ -2,7 +2,9 @@ use std::collections::HashMap;
 
 use super::errors::Errors;
 
-pub fn bytes_to_string_map(_bytes: &[u8]) -> Result<HashMap<String, String>, Errors> {
+
+// A [short] n, followed by n pair <k><v> where <k> and <v> are [string].
+pub fn bytes_to_string_map(bytes: &[u8]) -> Result<HashMap<String, String>, Errors> {
     let map = HashMap::new();
     Ok(map)
 }
