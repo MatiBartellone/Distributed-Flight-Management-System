3a6705204c4c08742238089ad5ce7607b64e3a34
thiagopservian <tpacheco@fi.uba.ar>
2024-10-05 13:12:30 -0300

    elminar comentarios creado

--- node/src/parsers/tokens/token.rs
+++ node/src/parsers/tokens/token.rs
@@ -48,6 +48,32 @@ enum DataType {
 }
 
 
+fn eliminar_between(input: &str, fin: &str, ini: &str) -> String {
+    let mut res = String::new();
+    let mut dentro_comentario = false;
+    for (i, c) in input.chars().enumerate() {
+        if dentro_comentario {
+            if i + 1 < input.len() && &input[i..i+2] == fin {
+                dentro_comentario = false;
+            }
+        } else {
+            if i + 1 < input.len() && &input[i..i+2] == ini {
+                dentro_comentario = true;
+            } else {
+                res.push(c);
+            }
+        }
+    }
+    res
+}
+
+
+fn eliminar_comentarios(input: &str) -> String {
+    let sin_diagonal = eliminar_between(input, "\n", "//");
+    let sin_barra = eliminar_between(&sin_diagonal, "--", "//");
+    eliminar_between(&sin_barra, "*/", "/*")
+}
+
 fn normalizar(entrada: &str) -> Vec<String> {
-    
+    let entrada = eliminar_comentarios(entrada);
 }
