919e41366a8dc8f9572ee940605baef95af5c3fb
thiagopservian <tpacheco@fi.uba.ar>
2024-10-06 00:36:04 -0300

    primera parte lexer funcionando

--- node/src/parsers/tokens/token.rs
+++ node/src/parsers/tokens/token.rs
@@ -68,11 +68,78 @@ fn eliminar_between(input: &str, fin: &str, ini: &str) -> String {
 }
 
 
+//me separa todas las partes que esten entre $, ' o ""
+//hola $hola como estas$ "bien" 'vos' el resto de el string
+//queda como ["hola ", "$hola como estas$", " "bien" ", "'vos' el resto de el string"]
+fn separar_secciones(input: &str) -> Vec<String> {
+    let mut res = Vec::new();
+    let mut seccion = String::new();
+    let mut delimitador: Option<char> = None;
+    let mut dentro_seccion = false;
+    for c in input.chars() {
+        if dentro_seccion {
+            if let Some(c) = delimitador {
+                seccion.push(c);
+                res.push(seccion);
+                seccion = String::new();
+                dentro_seccion = false;
+            } else {
+                seccion.push(c);
+            }
+
+        } else {
+            match c {
+                '$' | '"' | '\'' => {
+                    if !seccion.is_empty() {
+                        res.push(seccion); 
+                        seccion = String::new();
+                    }
+                    delimitador = Some(c);
+                    seccion.push(c);
+                } 
+                _ => seccion.push(c)
+                
+            }
+        }
+    }
+    if !seccion.is_empty() {
+        res.push(seccion);
+    }
+    res
+}
+
+//Elimina todos los comentarios de la query
 fn eliminar_comentarios(input: &str) -> String {
     let sin_diagonal = eliminar_between(input, "\n", "//");
     let sin_barra = eliminar_between(&sin_diagonal, "\n", "--");
     eliminar_between(&sin_barra, "*/", "/*")
 }
+
+fn separar_palabras(query: &str) -> Vec<String> {
+    let query = query
+        .replace("\n", " ")
+        .replace("\t", " ");
+    let query = query
+        .replace(">=", " _GE_ ") //Greater Equal (para que no se separen con los otros replace)
+        .replace("<=", " _LE_ ") //Less Equal
+        .replace("!=", " _DF_ ") 
+        .replace("<=", " _LE_ ") 
+        .replace("+", " + ")
+        .replace("-", " - ")
+        .replace("/", " / ")
+        .replace("%", " % ")
+        .replace("<", " < ")
+        .replace(">", " > ")
+        .replace("(", " ( ")
+        .replace(")", " ) ")
+        .replace(")", " , ")
+        .replace(";", "");
+    query
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect() 
+}
+
 
 fn normalizar(entrada: &str) -> Vec<String> {
     let entrada = eliminar_comentarios(entrada);
