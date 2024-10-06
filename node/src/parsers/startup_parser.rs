45a8a77f34a31e2cd5134caf843f389c83b4f220
Matias Bartellone <matias.bartellone@gmail.com>
2024-10-05 23:45:43 -0300

    Update startup_parser.rs

--- node/src/parsers/startup_parser.rs
+++ node/src/parsers/startup_parser.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-
+use crate::utils::bytes_cursor::BytesCursor;
 use crate::executables::executable::Executable;
 use crate::executables::startup_executable::StartupExecutable;
 use crate::parsers::parser::Parser;
