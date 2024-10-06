7ceb786478fb20c35b748bf177610dd4e1b65318
Matias Bartellone <matias.bartellone@gmail.com>
2024-10-05 23:47:19 -0300

    Update startup_parser.rs

--- node/src/parsers/startup_parser.rs
+++ node/src/parsers/startup_parser.rs
@@ -3,7 +3,6 @@ use crate::utils::bytes_cursor::BytesCursor;
 use crate::executables::executable::Executable;
 use crate::executables::startup_executable::StartupExecutable;
 use crate::parsers::parser::Parser;
-use crate::utils::conversion::bytes_to_string_map;
 use crate::utils::errors::Errors;
 
 const CQL_VERSION: &str = "CQL_VERSION";
