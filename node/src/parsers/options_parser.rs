1448d6a2c0d2a75ed4b06c70c61b1cc9c7b976fb
Emiliano Gomez <emiliano2805@gmail.com>
2024-10-05 23:23:33 -0300

    mini fix

--- node/src/parsers/options_parser.rs
+++ node/src/parsers/options_parser.rs
@@ -1,13 +1,7 @@
-use std::collections::HashMap;
-
 use crate::executables::executable::Executable;
 use crate::executables::options_executable::OptionsExecutable;
 use crate::parsers::parser::Parser;
 use crate::utils::errors::Errors;
-
-const ARGUMENTS_LENGTH: usize = 5;
-const CQL_VERSION: &str = "3.0.0";
-const COMPRESSION: &str = "lz4";
 
 pub struct OptionsParser;
 
@@ -20,22 +14,13 @@ impl OptionsParser {
         }
         Ok(())
     }
-
-    fn get_options(&self) -> HashMap<String, String> {
-        let mut options = HashMap::<String, String>::new();
-        options.insert("CQL_VERSION".to_string(), CQL_VERSION.to_string());
-        options.insert("COMPRESSION".to_string(), COMPRESSION.to_string());
-
-        options
-    }
 }
 
 impl Parser for OptionsParser {
-    fn parse(&self, _bytes: &[u8]) -> Result<Box<dyn Executable>, Errors> {
-        self.validate_empty_body(_bytes)?;
-        let options = self.get_options();
+    fn parse(&self, body: &[u8]) -> Result<Box<dyn Executable>, Errors> {
+        self.validate_empty_body(body)?;
 
-        let executable = OptionsExecutable::new(options);
+        let executable = OptionsExecutable::new();
         Ok(Box::new(executable))
     }
 }
@@ -45,7 +30,6 @@ mod tests {
     use super::*;
     fn setup() -> (OptionsParser, Vec<u8>) {
         let parser = OptionsParser;
-        let bytes = vec![];
         let bytes = vec![0x03, 0x00, 0x00, 0x01, 0x05];
         (parser, bytes)
     }
