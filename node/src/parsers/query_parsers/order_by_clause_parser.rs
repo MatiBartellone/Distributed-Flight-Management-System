26ad05fe3e5459095e8c85af9ab96ac8a2d43a44
MatiBartellone <matias.bartellone@gmail.com>
2024-10-07 16:03:31 -0300

    avance en parser

--- node/src/parsers/query_parsers/order_by_clause_parser.rs
+++ node/src/parsers/query_parsers/order_by_clause_parser.rs
@@ -10,18 +10,22 @@ pub struct OrderByClauseParser;
 impl OrderByClauseParser {
     pub fn parse(tokens: Vec<Token>) -> Result<Vec<OrderByClause>, Errors> {
         let mut order_clauses = Vec::<OrderByClause>::new();
-        column(&mut tokens.into_iter(), &mut order_clauses)?;
+        column(&mut tokens.into_iter(), &mut order_clauses, false)?;
         Ok(order_clauses)
     }
 }
 
-fn column(tokens: &mut IntoIter<Token>, order_clauses: &mut Vec<OrderByClause>) -> Result<(), Errors> {
+fn column(tokens: &mut IntoIter<Token>, order_clauses: &mut Vec<OrderByClause>, modified: bool) -> Result<(), Errors> {
     let Some(token) = tokens.next() else { return Ok(()) };
     match token {
         Token::Identifier(identifier)  => {
             order_clauses.push(OrderByClause::new(identifier));
-            order(tokens, order_clauses)?;
-            column(tokens, order_clauses)
+            column(tokens, order_clauses, false)
+        },
+        Token::Reserved(res) if res == *DESC || res == *ASC => {
+            if modified { return Err(Errors::SyntaxError(String::from("")))}
+            change_last_to_desc(order_clauses)?;
+            column(tokens, order_clauses, true)
         },
         _ => Err(Errors::SyntaxError(String::from("Unexpected token in order by clause"))),
     }
@@ -43,10 +47,12 @@ fn order(tokens: &mut IntoIter<Token>, order_clauses: &mut Vec<OrderByClause>) -
     }
 }
 
-fn change_last_to_desc(order_clauses: &mut Vec<OrderByClause>) {
+fn change_last_to_desc(order_clauses: &mut Vec<OrderByClause>, order: String) -> Result<(), Errors> {
+    if order_clauses.len() == 0 { return Err(Errors::SyntaxError(format!("No colum provided for {} modifier", order))) ; }
     let Some(order_clause) = order_clauses.pop();
-    order_clause.order = DESC.to_string();
+    order_clause.order = order;
     order_clauses.push(order_clause);
+    Ok(())
 }
 
 fn get_next_value(tokens: &mut IntoIter<Token>) -> Result<Token, Errors> {
