c7ece9c4fda2d5024a8fc549cf6567949a3faaca
Emiliano Gomez <emiliano2805@gmail.com>
2024-10-07 12:49:37 -0300

    refactor in execute method for executables

--- node/src/executables/auth_response_executable.rs
+++ node/src/executables/auth_response_executable.rs
@@ -2,6 +2,7 @@ use crate::auth::authenticator::Authenticator;
 use crate::executables::executable::Executable;
 use crate::frame::Frame;
 use crate::response_builders::frame_builder::FrameBuilder;
+use crate::utils::errors::Errors;
 use crate::utils::parser_constants::{AUTH_CHALLENGE, AUTH_SUCCESS};
 
 pub struct AuthResponseExecutable {
@@ -20,15 +21,18 @@ impl AuthResponseExecutable {
 }
 
 impl Executable for AuthResponseExecutable {
-    fn execute(&self, request: Frame) -> Frame {
-        // TODO: Sacar el unwrap
+    fn execute(&self, request: Frame) -> Result<Frame, Errors> {
         let user = self.user.to_string();
         let password = self.password.to_string();
         let ok = Authenticator::validate_credentials(user, password).unwrap();
+
         if ok {
             let body = self.get_token();
-            return FrameBuilder::build_response_frame(request, AUTH_SUCCESS, body).unwrap();
+            let ok_response = FrameBuilder::build_response_frame(request, AUTH_SUCCESS, body)?;
+            return Ok(ok_response);
         }
-        FrameBuilder::build_response_frame(request, AUTH_CHALLENGE, Vec::new()).unwrap()
+
+        let response = FrameBuilder::build_response_frame(request, AUTH_CHALLENGE, Vec::new())?;
+        return Ok(response);
     }
 }
--- node/src/executables/execute_executable.rs
+++ node/src/executables/execute_executable.rs
@@ -1,11 +1,11 @@
 use crate::executables::executable::Executable;
 use crate::frame::Frame;
+use crate::utils::errors::Errors;
 
 pub struct ExecuteExecutable;
 
 impl Executable for ExecuteExecutable {
-    fn execute(&self, _request: Frame) -> Frame {
+    fn execute(&self, _request: Frame) -> Result<Frame, Errors> {
         todo!()
     }
 }
-
