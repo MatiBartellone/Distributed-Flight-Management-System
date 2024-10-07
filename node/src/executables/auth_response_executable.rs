0e0d48bb1d026624fe0df40931170014c7c750c6
Emiliano Gomez <emiliano2805@gmail.com>
2024-10-06 23:55:05 -0300

    skel for auth parser and executable done

--- node/src/executables/auth_response_executable.rs
+++ node/src/executables/auth_response_executable.rs
@@ -1,11 +1,35 @@
+use crate::auth::authenticator::Authenticator;
 use crate::executables::executable::Executable;
 use crate::frame::Frame;
+use crate::response_builders::frame_builder::FrameBuilder;
 
-pub struct AuthResponseExecutable;
+const AUTH_SUCCESS: u8 = 0x10;
+const AUTH_CHALLENGE: u8 = 0x0E;
+pub struct AuthResponseExecutable {
+    user: String,
+    password: String,
+}
 
-impl Executable for AuthResponseExecutable {
-    fn execute(&self, _request: Frame) -> Frame {
-        todo!()
+impl AuthResponseExecutable {
+    pub fn new(user: String, password: String) -> AuthResponseExecutable {
+        AuthResponseExecutable { user, password }
+    }
+
+    fn get_token(&self) -> Vec<u8> {
+        Vec::<u8>::new()
     }
 }
 
+impl Executable for AuthResponseExecutable {
+    fn execute(&self, request: Frame) -> Frame {
+        // TODO: Sacar el unwrap
+        let user = self.user.to_string();
+        let password = self.password.to_string();
+        let ok = Authenticator::validate_credentials(user, password).unwrap();
+        if ok {
+            let body = self.get_token();
+            return FrameBuilder::build_response_frame(request, AUTH_SUCCESS, body).unwrap();
+        }
+        FrameBuilder::build_response_frame(request, AUTH_CHALLENGE, Vec::new()).unwrap()
+    }
+}
--- node/src/parsers/auth_response_parser.rs
+++ node/src/parsers/auth_response_parser.rs
@@ -1,13 +1,46 @@
+use core::str;
+
 use crate::executables::auth_response_executable::AuthResponseExecutable;
 use crate::executables::executable::Executable;
 use crate::parsers::parser::Parser;
+use crate::utils::bytes_cursor::BytesCursor;
 use crate::utils::errors::Errors;
 
-pub struct AuthResponseParser;
+pub struct AuthResponseParser {}
 
-impl Parser for AuthResponseParser {
-    fn parse(&self, _body: &[u8]) -> Result<Box<dyn Executable>, Errors> {
-        Ok(Box::new(AuthResponseExecutable))
+impl AuthResponseParser {
+    fn valid_credentials(&self, body: &[u8]) -> Result<Option<Vec<u8>>, Errors> {
+        let mut cursor = BytesCursor::new(body);
+        let credentials_as_bytes = cursor.read_bytes()?;
+        cursor.read_remaining_bytes()?;
+        Ok(credentials_as_bytes)
+    }
+
+    fn get_user_and_password(
+        &self,
+        credentials: Option<Vec<u8>>,
+    ) -> Result<(String, String), Errors> {
+        let credentials =
+            credentials.ok_or(Errors::ProtocolError("Invalid credentials".to_string()))?;
+        let credentials = str::from_utf8(&credentials)
+            .map_err(|_| Errors::ProtocolError("Invalid credentials".to_string()))?;
+        let mut credentials_split = credentials.split(':');
+
+        let user = credentials_split.next().ok_or(Errors::ProtocolError(
+            "Missing user in credentials".to_string(),
+        ))?;
+        let password = credentials_split.next().ok_or(Errors::ProtocolError(
+            "Missing password in credentials".to_string(),
+        ))?;
+
+        Ok((user.to_string(), password.to_string()))
     }
 }
+impl Parser for AuthResponseParser {
+    fn parse(&self, body: &[u8]) -> Result<Box<dyn Executable>, Errors> {
+        let credentials = self.valid_credentials(body)?;
+        let (user, password) = self.get_user_and_password(credentials)?;
 
+        Ok(Box::new(AuthResponseExecutable::new(user, password)))
+    }
+}
