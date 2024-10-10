e642cfbfab68332be27d78914704a463cdeb1368
MatiBartellone <matias.bartellone@gmail.com>
2024-10-10 14:19:02 -0300

    create table v3

--- node/src/parsers/query_parsers/create_table_query_parser.rs
+++ node/src/parsers/query_parsers/create_table_query_parser.rs
@@ -13,6 +13,7 @@ impl CreateTableQueryParser {
     pub fn parse(tokens: Vec<Token>) -> Result<CreateTableQuery, Errors> {
         let mut create_table_query = CreateTableQuery::new();
         table(&mut tokens.into_iter(), &mut create_table_query)?;
+        check_primary_key(&mut create_table_query)?;
         Ok(create_table_query)
     }
 }
@@ -39,7 +40,15 @@ fn table_name(tokens: &mut IntoIter<Token>, query: &mut CreateTableQuery) -> Res
 
 fn column_list(tokens: &mut IntoIter<Token>, query: &mut CreateTableQuery) -> Result<(), Errors> {
     match get_next_value(tokens)? {
-        Token::TokensList(list) => column(&mut list.into_iter(), query),
+        Token::TokensList(list) => {
+            column(&mut list.into_iter(), query)?;
+            let None = tokens.next() else {
+                return Err(Errors::SyntaxError(String::from(
+                    "Nothing should follow the column list",
+                )));
+            };
+            Ok(())
+        },
         _ => Err(Errors::SyntaxError(String::from(
             "Unexpected token in column definition",
         ))),
@@ -47,20 +56,27 @@ fn column_list(tokens: &mut IntoIter<Token>, query: &mut CreateTableQuery) -> Re
 }
 
 fn column(tokens: &mut IntoIter<Token>, query: &mut CreateTableQuery) -> Result<(), Errors> {
-    match get_next_value(tokens)? {
+    let Some(token) = tokens.next() else {
+        return Ok(());
+    };
+    match token {
         Token::Identifier(identifier) => {
-            query.columns.insert(identifier, get_data_type(tokens)?);
-            match get_next_value(tokens)? {
-                Token::Identifier(i) if i == *"," => column(tokens, query),
-                Token::Reserved(res) if res == *PRIMARY => {
-                    primary_key_def(tokens, query, &identifier)
-                }
-                _ => Err(Errors::SyntaxError(String::from(
-                    "Unexpected token in column definition",
-                ))),
-            }
+            query.columns.insert(identifier.clone(), get_data_type(tokens)?);
+            try_primary_key(tokens, query, identifier)
         }
         Token::Reserved(res) if res == *PRIMARY => key(tokens, query),
+        _ => Err(Errors::SyntaxError(String::from(
+            "Unexpected token in column definition",
+        ))),
+    }
+}
+
+fn try_primary_key(tokens: &mut IntoIter<Token>, query: &mut CreateTableQuery, primary_key: String) -> Result<(), Errors> {
+    match get_next_value(tokens)? {
+        Token::Identifier(i) if i == *"," => column(tokens, query),
+        Token::Reserved(res) if res == *PRIMARY => {
+            primary_key_def(tokens, query, primary_key)
+        }
         _ => Err(Errors::SyntaxError(String::from(
             "Unexpected token in column definition",
         ))),
@@ -70,11 +86,11 @@ fn column(tokens: &mut IntoIter<Token>, query: &mut CreateTableQuery) -> Result<
 fn primary_key_def(
     tokens: &mut IntoIter<Token>,
     query: &mut CreateTableQuery,
-    primary_key: &String,
+    primary_key: String,
 ) -> Result<(), Errors> {
     match get_next_value(tokens)? {
         Token::Reserved(res) if res == *KEY => {
-            query.primary_key = primary_key.to_string();
+            set_primary_key(query, primary_key)?;
             match get_next_value(tokens)? {
                 Token::Identifier(i) if i == *"," => Ok(()),
                 _ => Err(Errors::SyntaxError(String::from(
@@ -109,7 +125,7 @@ fn primary_key_list(
             };
             match list.first() {
                 Some(Token::Identifier(identifier)) => {
-                    query.primary_key = identifier.to_string();
+                    set_primary_key(query, identifier.to_string())?;
                     Ok(())
                 }
                 _ => Err(Errors::SyntaxError(String::from(
@@ -136,4 +152,22 @@ fn get_next_value(tokens: &mut IntoIter<Token>) -> Result<Token, Errors> {
     tokens
         .next()
         .ok_or(Errors::SyntaxError(String::from("Query lacks parameters")))
+}
+
+fn set_primary_key(query: &mut CreateTableQuery, primary_key: String) -> Result<(), Errors> {
+    if query.primary_key.is_empty() {
+        query.primary_key = primary_key;
+        return Ok(())
+    }
+    Err(Errors::SyntaxError(String::from("Primary key must be defined once")))
+}
+
+fn check_primary_key(query: &mut CreateTableQuery) -> Result<(), Errors> {
+    if query.columns.get(query.primary_key.as_str()).is_none() {
+        return Err(Errors::SyntaxError(String::from("Primary key column not defined")))
+    }
+    if query.primary_key.is_empty() {
+        return Err(Errors::SyntaxError(String::from("Primary not defined")))
+    }
+    Ok(())
 }
