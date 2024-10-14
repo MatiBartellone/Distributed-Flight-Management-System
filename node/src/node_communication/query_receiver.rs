0019de21e434fabfe4274d9a97e38b399d721f1e
MatiBartellone <matias.bartellone@gmail.com>
2024-10-13 21:20:12 -0300

    query receiver

--- node/src/node_communication/query_receiver.rs
+++ node/src/node_communication/query_receiver.rs
@@ -11,13 +11,17 @@ pub struct QueryReceiver {}
 
 impl QueryReceiver {
     pub fn start_listening(&self) {
-        let listener = TcpListener::bind(""/*get_own_node_comms_ip()*/).unwrap();
+        let listener = TcpListener::bind(""/*get_own_node_comms_ip():NODES_PORT*/).unwrap();
 
         for incoming in listener.incoming() {
             match incoming {
                 Ok(stream) => {
                     thread::spawn(move || {
-                        self.handle_query(stream);
+                        if let Ok(response) = self.handle_query(&stream) {
+                            self.respond_to_request(stream, response);
+                        } else {
+                            self.respond_to_request(stream, String::from("error"));
+                        };
                     });
                 },
                 Err(e) => {}
@@ -25,16 +29,21 @@ impl QueryReceiver {
         }
     }
 
-    fn handle_query(&self, mut stream: TcpStream) {
+    fn handle_query(&self, mut stream: &TcpStream) -> Result<String, Errors> {
         let mut buffer = [0; 1024];
         match stream.read(&mut buffer) {
             Ok(_) => {
                 let query = QuerySerializer::deserialize(&buffer);
-                query.run()
+                let response = query.run()?;
+                Ok(response)
             }
             Err(e) => {
                 Err(Errors::ServerError(String::from("")))
             }
         }
+    }
+
+    fn respond_to_request(&self, mut stream: TcpStream, response: String) {
+        stream.write(response.as_bytes()).unwrap();
     }
 }
