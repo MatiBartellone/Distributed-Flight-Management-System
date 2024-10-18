978de5417aca430a7ddf30e4dcddfc3475a3cb6a
thiagopservian <tpacheco@fi.uba.ar>
2024-10-18 19:34:59 -0300

    nodeMetaData y hashing hecho. Faltan test

--- node/src/meta_data/nodes/node_meta_data_acces.rs
+++ node/src/meta_data/nodes/node_meta_data_acces.rs
@@ -1,31 +1,60 @@
+use std::fs::{File, OpenOptions};
+
+use crate::utils::errors::Errors;
+
+use super::{node_group::NodeGroup, node::{Node, self}};
+
+use twox_hash::XxHash32;
+use std::hash::{Hasher};
+
+
 
 
 #[derive(Debug)]
-pub struct NodesMetaDataAccess {
-    file: Arc<Mutex<File>>, // Compartir el Mutex
-}
+pub struct NodesMetaDataAccess;
 
 impl NodesMetaDataAccess {
-    pub fn new(path: &str, node: Node) -> Result<Self, Errors> {
+    fn open(path: &str) -> Result<File, Errors> {
         let file = OpenOptions::new()
         .read(true)  // Permitir lectura
         .write(true) // Permitir escritura
         .create(true) // Crear el archivo si no existe
         .open(path)
         .map_err(|_| Errors::ServerError("Unable to open or create file".to_string()))?;
-        let mutex = Arc::new(Mutex::new(file));
-        Ok(Self { file: mutex })
+        Ok(file)
     }
 
+    fn read_node_group(path: &str) -> Result<NodeGroup, Errors> {
+        let mut file = Self::open(path)?;
+        let node_group: NodeGroup = serde_json::from_reader(&file)
+            .map_err(|_| Errors::ServerError("Failed to read or deserialize NodeGroup".to_string()))?;
+        Ok(node_group)
+    }
 
-    fn lock_and_extract_keyspaces(
-        &self,
-    ) -> Result<(MutexGuard<File>, HashMap<String, Keyspace>), Errors> {
-        let mut file = self
-            .file
-            .lock()
-            .map_err(|_| Errors::ServerError("Failed to acquire lock".to_string()))?;
-        let keyspaces = self.extract_hash_from_json(&mut file)?;
-        Ok((file, keyspaces))
+    fn write_node_group(path: &str, node_group: &NodeGroup) -> Result<(), Errors> {
+        let file = Self::open(path)?;
+        serde_json::to_writer(&file, &node_group)
+            .map_err(|_| Errors::ServerError("Failed to write NodeGroup to file".to_string()))?;
+        Ok(())
+    }
+
+    //el special node es el nodo en el que estamos
+    pub fn get_special_node_ip(path: &str) -> Result<String, Errors> {
+        let node_group = Self::read_node_group(path)?;
+        Ok(node_group.ip_principal().to_string())
+    }
+
+    //Si no hay que delegar, retorna None
+    pub fn get_delegation(path: &str, key: String)-> Result<Option<Node>, Errors> {
+        let hasshing_key = hash_string_murmur3(&key);
+        let node_group = Self::read_node_group(path)?;
+        let pos = hasshing_key % node_group;
+        Ok(node_group.get_node(pos))
     }
 }
+
+fn hash_string_murmur3(input: &str) -> usize {
+        let mut hasher = XxHash32::with_seed(0); 
+        hasher.write(input.as_bytes());
+        hasher.finish() as usize
+    }
