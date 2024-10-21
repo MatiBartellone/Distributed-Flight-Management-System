f0740a2d8d07780a1310b1914a08e6c736ce31ae
MatiBartellone <matias.bartellone@gmail.com>
2024-10-21 18:19:39 -0300

    drop_table, alter_table, select_rows casi terminado y un poco de modularizacion

--- node/src/data_access/data_access.rs
+++ node/src/data_access/data_access.rs
@@ -1,8 +1,11 @@
 use crate::data_access::row::Row;
 use crate::utils::errors::Errors;
-use std::fs::{rename, File, OpenOptions};
+use std::fs::{remove_file, rename, File, OpenOptions};
 use std::io::{BufReader, Read, Seek, SeekFrom, Write};
+use serde_json::de::IoRead;
+use serde_json::StreamDeserializer;
 use crate::queries::evaluate::Evaluate;
+use crate::queries::order_by_clause::OrderByClause;
 use crate::queries::where_logic::where_clause::WhereClause;
 
 pub struct DataAccess {}
@@ -13,19 +16,26 @@ impl DataAccess {
     }
 
     pub fn create_table(&self, table_name: &String) -> Result<(), Errors> {
-        let file = self.open_file(&table_name);
-
+        let path = self.get_file_path(table_name);
+        let file = self.open_file(&path);
         if file.is_ok() {
             return Err(Errors::AlreadyExists("Table already exists".to_string()));
         }
-
-        File::create(format!("{}.json", table_name))
+        File::create(path)
             .map_err(|_| Errors::ServerError(String::from("Could not create file")))?;
         Ok(())
     }
 
-    pub fn alter(&self, table_name: String) -> Result<(), Errors> {
-        let file = self.open_file(&table_name)?;
+    pub fn alter_table(&self, table_name: String) -> Result<(), Errors> {
+        let _file = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(self.get_file_path(&table_name)).map_err(|_| Errors::ServerError(String::from("Could not open file")))?;
+        Ok(())
+    }
+
+    pub fn drop_table(&self, table_name: String) -> Result<(), Errors> {
+        remove_file(self.get_file_path(&table_name)).map_err(|_| Errors::ServerError(String::from("Could not remove file")))?;
         Ok(())
     }
 
@@ -40,12 +50,9 @@ impl DataAccess {
     }
 
     pub fn update_row(&self, path: &String, new_row: Row, where_clause: WhereClause) -> Result<(), Errors> {
-        let file = self.open_file(path)?;
         let temp_path = format!("{}.tmp", path);
         self.create_table(&temp_path)?;
-        let reader = BufReader::new(file);
-        let stream = serde_json::Deserializer::from_reader(reader).into_iter::<Row>();
-        for row in stream {
+        for row in self.get_deserialized_stream(path)? {
             match row {
                 Ok(row) => {
                     if where_clause.evaluate(&row.get_row_hash())?{
@@ -59,6 +66,47 @@ impl DataAccess {
         }
         rename(temp_path, path).map_err(|_| Errors::ServerError(String::from("Error renaming file")))?;
         Ok(())
+    }
+
+    pub fn select_rows(&self, table_name: &String, where_clause: WhereClause, order_by_clause: OrderByClause) -> Result<Vec<Row>, Errors> {
+        let path = self.get_file_path(table_name);
+        let filtered_path = self.get_file_path(&String::from("filtered"));
+        self.filter_rows(&path, &filtered_path, where_clause)?;
+        self.sort_rows(&filtered_path, order_by_clause)?;
+        let rows = self.get_rows(&filtered_path)?;
+        remove_file(filtered_path).map_err(|_| Errors::ServerError(String::from("Could not remove file")))?;
+        Ok(rows)
+    }
+
+    fn filter_rows(&self, path: &String, filtered_path: &String, where_clause: WhereClause) -> Result<(), Errors> {
+        for row in self.get_deserialized_stream(path)? {
+            match row {
+                Ok(row) => {
+                    if where_clause.evaluate(&row.get_row_hash())?{
+                        self.append_row(&filtered_path, &row)?;
+                    }
+                }
+                Err(_) => return Err(Errors::ServerError(String::from("Error deserializing row"))),
+            }
+        }
+        Ok(())
+    }
+
+    fn sort_rows(&self, path: &String, order_by_clause: OrderByClause) -> Result<(), Errors> {
+        Ok(())
+    }
+
+    fn get_rows(&self, path: &String) -> Result<Vec<Row>, Errors> {
+        let mut rows = Vec::new();
+        for row in self.get_deserialized_stream(path)? {
+            match row {
+                Ok(row) => {
+                    rows.push(row);
+                }
+                Err(_) => return Err(Errors::ServerError(String::from("Error deserializing row"))),
+            }
+        }
+        Ok(rows)
     }
 
     fn get_file_path(&self, table_name: &String) -> String {
@@ -86,10 +134,7 @@ impl DataAccess {
     }
 
     fn pk_already_exists(&self, path: &String, primary_keys: &Vec<String>) -> Result<bool, Errors> {
-        let file = self.open_file(path)?;
-        let reader = BufReader::new(file);
-        let stream = serde_json::Deserializer::from_reader(reader).into_iter::<Row>();
-        for row in stream {
+        for row in self.get_deserialized_stream(path)? {
             match row {
                 Ok(row) => {
                     if &row.primary_keys == primary_keys {
@@ -100,5 +145,11 @@ impl DataAccess {
             }
         }
         Ok(false)
+    }
+
+    fn get_deserialized_stream(&self, path: &String) -> Result<StreamDeserializer<IoRead<BufReader<File>>, Row>, Errors> {
+        let file = self.open_file(path)?;
+        let reader = BufReader::new(file);
+        Ok(serde_json::Deserializer::from_reader(reader).into_iter::<Row>())
     }
 }
