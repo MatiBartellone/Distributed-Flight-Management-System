509ccd27ee115bb3f5c8e6c232ee625889f7faab
MatiBartellone <matias.bartellone@gmail.com>
2024-11-25 11:53:32 -0300

    node_handler funcional

--- node_handler/src/main.rs
+++ node_handler/src/main.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use std::io::{Read, Write};
-use std::net::{IpAddr, TcpListener, TcpStream};
-use std::sync::{Arc, Mutex};
-use std::{io, thread};
+use std::io::Write;
+use std::net::IpAddr;
+use std::io;
+use std::fmt::Display;
+use std::thread::sleep;
+use std::time::Duration;
 use termion::{color, style};
-use termion::color::{Color, Fg};
-// Para el color verde
+
 
 fn clear_screen() {
     print!("\x1B[2J\x1B[1;1H");
@@ -36,12 +36,20 @@ fn print_node_list(nodes_vec: &mut Vec<Node>) {
             State::Booting => Box::new(color::Blue),
         };
 
+        let (seed_color, is_seed): (Box<dyn termion::color::Color>, &str) = match node.is_seed {
+            true => (Box::new(color::Green), "Y"),
+            false => (Box::new(color::Red), "N")
+        };
+
         println!(
-            "IP: {} | Port: {} | Position: {} - {}STATE: {}{}",
+            "IP: {} | Port: {} | Position: {} | is_seed: {}{}{} | STATE: {}{}{}",
             node.ip.ip.to_string(),
             node.ip.port.to_string(),
             node.position,
-            color,
+            color::Fg(&*seed_color),
+            is_seed,
+            color::Fg(color::Reset),
+            color::Fg(&*color),
             node.state.to_string(),
             color::Fg(color::Reset)
         );
@@ -56,14 +64,24 @@ fn main() {
     io::stdin()
         .read_line(&mut path)
         .expect("Error reading data");
-    path.trim().to_string();
+    path = path.trim().to_string();
+    dbg!(&path);
+
+    loop {
+        let p = path.clone();
+        print_node_list(&mut read_cluster(p.as_str()));
+        sleep(Duration::from_secs(1))
+    }
 
 }
 
-fn read_cluster(path: &str) -> Cluster {
+fn read_cluster(path: &str) -> Vec<Node> {
     let content = std::fs::read_to_string(path).expect("Failed to read file");
     let cluster: Cluster = serde_json::from_slice(content.as_bytes()).expect("Failed to deserialize");
-    cluster
+    let mut nodes_vec: Vec<Node> = Vec::new();
+    nodes_vec.extend(cluster.other_nodes);
+    nodes_vec.push(cluster.own_node);
+    nodes_vec
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -77,6 +95,16 @@ pub enum State {
     Active,
     Inactive,
     Booting,
+}
+
+impl Display for State {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        match self {
+            State::Active => write!(f, "Active"),
+            State::Inactive => write!(f, "Inactive"),
+            State::Booting => write!(f, "Booting"),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
