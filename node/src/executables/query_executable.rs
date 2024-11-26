ebfb2ab408981cf64c3bc51399111945273e8d24
MatiBartellone <matias.bartellone@gmail.com>
2024-11-26 11:29:18 -0300

    use funcionando

--- node/src/executables/query_executable.rs
+++ node/src/executables/query_executable.rs
@@ -28,12 +28,17 @@ impl Executable for QueryExecutable {
         let Some(query_enum) = QueryEnum::from_query(&self.query) else {
             return Err(Errors::ServerError(String::from("")));
         };
-        let delegator = QueryDelegator::new(
-            pk,
-            query_enum.into_query(),
-            ConsistencyLevel::from_i16(self.consistency_integer)?,
-        );
-        let response_msg = delegator.send()?;
+        let response_msg = match query_enum {
+            QueryEnum::Use(use_query) => use_query.run()?,
+            _ => {
+                let delegator = QueryDelegator::new(
+                    pk,
+                    query_enum.into_query(),
+                    ConsistencyLevel::from_i16(self.consistency_integer)?,
+                );
+                delegator.send()?
+            }
+        };
         let response_frame = FrameBuilder::build_response_frame(request, RESULT, response_msg)?;
         Ok(response_frame)
     }
