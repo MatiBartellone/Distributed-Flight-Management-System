526c26b8f1a9328786ef91556c7562df61daece1
ivan-maximoff <maximoff.ivan@gmail.com>
2024-11-26 21:14:53 -0300

    TLS con tokio-rustls

--- ui/src/cassandra_comunication/cassandra_client.rs
+++ ui/src/cassandra_comunication/cassandra_client.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use tokio::runtime::Runtime;
+
 use crate::utils::{
         consistency_level::ConsistencyLevel, constants::{OP_AUTHENTICATE, OP_AUTH_CHALLENGE, OP_AUTH_RESPONSE, OP_AUTH_SUCCESS}, frame::Frame, system_functions::get_user_data, types_to_bytes::TypesToBytes
     };
@@ -18,13 +20,18 @@ pub struct CassandraClient {
 impl CassandraClient {
     /// Creates a new CassandraClient with the given node
     pub fn new(node: &str) -> Result<Self, String> {
-        let connection = CassandraConnection::new(node)?;
+        let connection = Runtime::new()
+            .map_err(|e| format!("Failed to create runtime: {}", e))?
+            .block_on(CassandraConnection::new(node))?;
         Ok(Self { connection })
     }
     
     /// Send a frame to the server and returns the response
     pub fn send_and_receive(&mut self, frame: &mut Frame) -> Result<Frame, String> {
-        self.connection.send_and_receive(frame)
+        let result = Runtime::new()
+            .map_err(|e| format!("Failed to create runtime: {}", e))?
+            .block_on(self.connection.send_and_receive(frame));
+        result
     }
 
     /// Get ready the client for use in keyspace airport
--- ui/src/cassandra_comunication/cassandra_connector.rs
+++ ui/src/cassandra_comunication/cassandra_connector.rs
@@ -1,20 +1,22 @@
+use rustls_pki_types::ServerName;
+use tokio_rustls::client::TlsStream;
 use webpki_roots::TLS_SERVER_ROOTS;
-use std::io::Read;
-use std::{io::Write, sync::Arc};
-use std::net::TcpStream;
+use std::sync::Arc;
 use crate::utils::frame::Frame;
 
-use rustls::{ClientConfig, ClientConnection, RootCertStore, Stream};
+use rustls::{ClientConfig, RootCertStore};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+use tokio_rustls::TlsConnector;
 
 pub struct CassandraConnection {
-    stream: Stream<'static, ClientConnection, TcpStream>
+    stream: TlsStream<TcpStream>,
 }
 
 impl CassandraConnection {
     /// Create a new connection to the server with the given node (ip:port)
-    pub fn new(node: &str) -> Result<Self, String> {
-        let ip = node.split_once(":")
-            .map(|(ip, _)| ip.to_string())
+    pub async  fn new(node: &str) -> Result<Self, String> {
+        let (ip, _port) = node.split_once(":")
             .ok_or_else(|| "Formato incorrecto de la cadena ip:port".to_string())?;
         
         // Get the ClientConfig
@@ -22,44 +24,49 @@ impl CassandraConnection {
         let rc_config = Arc::new(config);
 
         // Create a new connection to the server
-        let ip = ip.try_into()
-            .map_err(|_| "Invalid IP".to_string())?;
-        let mut conn = ClientConnection::new(rc_config, ip)
-            .map_err(|e| format!("Error creating the connection: {}", e))?;
+        let connector = TlsConnector::from(rc_config);
 
         // Connect to the server with a socket
-        let mut socket = TcpStream::connect(node)
+        let socket = TcpStream::connect(node)
+            .await
             .map_err(|e| format!("Error connecting to {}: {}", node, e))?;
 
-        // Create a new Stream with the connection and the socket
-        let tls_conn= Stream::new(&mut conn, &mut socket);
+        // Create a new Stream with the connector and the socket
+        let domain = ServerName::try_from(ip)
+            .map_err(|_| format!("Error converting IP {} to ServerName", ip))?
+            .to_owned();
+        let stream = connector.connect(domain, socket)
+            .await
+            .map_err(|e| format!("Error connecting to {}: {}", node, e))?;
         
-        Ok(Self { stream: tls_conn })
+        Ok(Self { stream })
     }
 
-    fn write_stream(&mut self, frame: &Frame) -> Result<(), String> {
+    async fn write_stream(&mut self, frame: &Frame) -> Result<(), String> {
         let frame = frame.to_bytes().map_err(|_| "Error al convertir a bytes".to_string())?;
         
         self.stream.write_all(&frame)
+            .await
             .map_err(|_| "Error al escribir".to_string())?;
 
         self.stream.flush()
+            .await
             .map_err(|_| "Error al hacer flush".to_string())?;
         Ok(())
     }
 
-    fn read_stream(&mut self) -> Result<Frame, String> {
+    async fn read_stream(&mut self) -> Result<Frame, String> {
         let mut buf = [0; 1024];
-        match self.stream.read(&mut buf) {
+        match self.stream.read(&mut buf).await {
             Ok(n) if n > 0 => Frame::parse_frame(&buf[..n]),
             _ => Err("Fail reading the response".to_string()),
         }
     }
     
     /// Send a frame to the server and wait for the response
-    pub fn send_and_receive(& mut self, frame: &Frame) -> Result<Frame, String> {
-        self.write_stream(&frame)?;
-        self.read_stream()
+    pub async fn send_and_receive(& mut self, frame: &Frame) -> Result<Frame, String> {
+        self.write_stream(&frame).await?;
+        self.read_stream().await
     }
 
     fn get_config() -> Result<ClientConfig, String> {
