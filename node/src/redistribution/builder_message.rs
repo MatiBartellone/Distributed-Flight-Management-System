2d3212d002652bce616faf0940bed11c19953bc8
thiagopservian <tpacheco@fi.uba.ar>
2025-01-31 20:16:46 -0300

    create table hecho

--- node/src/redistribution/builder_message.rs
+++ node/src/redistribution/builder_message.rs
@@ -1,4 +1,6 @@
-use crate::{utils::{errors::Errors, types::token_conversor::{create_reserved_token, create_identifier_token, create_symbol_token, create_token_from_literal, create_paren_list_token, create_iterate_list_token, create_comparison_operation_token, create_token_literal, create_logical_operation_token, create_brace_list_token}, constants::{KEYSPACE_METADATA_PATH, REPLICATION}, parser_constants::COMMA}, data_access::{row::Row, column::{Column, self}}, queries::query::Query, parsers::{tokens::{token::Token, literal::create_literal}, query_parser::query_parser}, read_reparation::utils::to_hash_columns, meta_data::meta_data_handler::use_keyspace_meta_data};
+use std::collections::HashMap;
+
+use crate::{utils::{errors::Errors, types::{token_conversor::{create_reserved_token, create_identifier_token, create_symbol_token, create_token_from_literal, create_paren_list_token, create_iterate_list_token, create_comparison_operation_token, create_token_literal, create_logical_operation_token, create_brace_list_token, create_data_type_token}, primary_key::PrimaryKey}, constants::{KEYSPACE_METADATA_PATH, REPLICATION}, parser_constants::COMMA}, data_access::{row::Row, column::{Column, self}}, queries::query::Query, parsers::{tokens::{token::Token, literal::create_literal}, query_parser::query_parser}, read_reparation::utils::to_hash_columns, meta_data::meta_data_handler::use_keyspace_meta_data};
 use crate::parsers::tokens::terms::ComparisonOperators::Equal;
 use crate::parsers::tokens::terms::LogicalOperators::And;
 use crate::parsers::tokens::{data_type::DataType, literal::Literal};
@@ -18,6 +20,11 @@ impl BuilderMessage {
 
     pub fn build_keyspace(keyspace: String) -> Result<Box<dyn Query>, Errors> {
         let query = BuilderMessage::create_query_keyspace(keyspace)?;
+        query_parser(query)
+    }
+
+    pub fn build_table(path: String) -> Result<Box<dyn Query>, Errors> {
+        let query = BuilderMessage::create_query_table(path)?;
         query_parser(query)
     }
 
@@ -86,6 +93,15 @@ impl BuilderMessage {
         Ok(query)
     }
 
+    fn create_query_table(table: String) -> Result<Vec<Token>, Errors> {
+        let mut query: Vec<Token> = Vec::new();
+        query.push(create_reserved_token("CREATE"));
+        query.push(create_reserved_token("TABLE"));
+        query.push(create_identifier_token(&table));
+        query.push(paren_list(&table)?);
+        Ok(query)
+    }
+
 }
 
 
@@ -113,4 +129,44 @@ fn get_meta_data_keyspace(keyspace: &str) -> Result<(String, String), Errors> {
         handler.get_strategy(KEYSPACE_METADATA_PATH.to_owned(), keyspace)
     })?;
     Ok((replication.to_string(), strategy))
+}
+
+
+fn paren_list(path: &str) -> Result<Token, Errors> {
+    let (keyspace, table) = path.split_once('.').ok_or_else(|| Errors::ServerError("Failed to read keyspace.table".to_string()))?;
+    let (pks, columns) = get_meta_data_table(keyspace, table)?;
+    let mut list: Vec<Token> = Vec::new();
+    for column in columns {
+        list.push(create_identifier_token(&column.0));
+        list.push(create_data_type_token(column.1));
+        list.push(create_symbol_token(COMMA));
+    }
+    list.push(create_reserved_token("PRIMARY"));
+    list.push(create_reserved_token("KEY"));
+    list.push(sub_list(pks));
+    Ok(create_paren_list_token(list))
+}
+
+
+fn sub_list(pks: Vec<String>) -> Token {
+    let mut list: Vec<Token> = Vec::new();
+    for (i, pk) in pks.clone().into_iter().enumerate() {
+        list.push(create_identifier_token(&pk));
+
+        if i < pks.len() - 1 {
+            list.push(create_symbol_token(COMMA));
+        }
+    }
+    create_paren_list_token(list)
+}
+
+
+fn get_meta_data_table(keyspace: &str, table: &str) -> Result<(Vec<String>, HashMap<String, DataType>), Errors> {
+    let header_pks = use_keyspace_meta_data(|handler| {
+        handler.get_primary_key( KEYSPACE_METADATA_PATH.to_owned(), keyspace, table)
+    })?;
+    let columns = use_keyspace_meta_data(|handler| {
+        handler.get_columns_type( KEYSPACE_METADATA_PATH.to_owned(), keyspace, table)
+    })?;
+    Ok((header_pks.get_full_primary_key(), columns))
 }
